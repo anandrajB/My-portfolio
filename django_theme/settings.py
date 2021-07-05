90d4abb71e52f2174ee13aebe82d92de439b8478
anandrajB <anand98.ar@gmail.com>
2021-07-05 10:52:39 +0530

    gmail smtp

--- django_theme/settings.py
+++ django_theme/settings.py
@@ -146,8 +146,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "thesingleapp2019@gmail.com"
-EMAIL_HOST_PASSWORD = "TheSingleApp@2019"
+EMAIL_HOST_USER = "dessschatbot@gmail.com"
+EMAIL_HOST_PASSWORD = "btuzerrlavbbmorw"
+
+
 
 #maintanance mode
 #True --> on   ,  None --> OFF
